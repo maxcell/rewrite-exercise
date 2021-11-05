96a8a40a938f2f24c2e9c183757db1424c43ff96
Prince Wilson <maxcell.wilson@gmail.com>
2021-11-05 12:16:09 -0400

    feat: Refactor into function to handle processing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+use std::error::Error;
+
 use lol_html::{HtmlRewriter, Settings, element};
 
-fn main() {
-    // Initial Exercise:
-    // Using lol_html, find all image tags and an attribute of
-    // loading with the value "lazy".
+fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("index.html"); 
-    
-    let mut output = vec![];
+    let output = process_input(input);
+    println!("{}", output?);
+
+    Ok(())
+}
+
+// Second problem we pivoted:
+// Instead of doing all the work with unwraps:
+// How can we make sure to do this with a function and idiomatically handle
+// the errors along the way
+fn process_input(input: &str) -> Result<String, Box<dyn Error>> {
+  let mut output = vec![];
+
     let mut rewriter = HtmlRewriter::try_new(
         Settings {
             element_content_handlers: vec![
                 element!("img[src]", |el| { 
-                    el.set_attribute("loading", "lazy").unwrap();
+                    el.set_attribute("loading", "lazy")?;
 
                     Ok(())
                 })
@@ -19,17 +29,9 @@ fn main() {
             ..Settings::default()
         },
         |c: &[u8]| output.extend_from_slice(c)
-    ).unwrap();
+    )?;
+    rewriter.write(input.as_bytes())?;
+    rewriter.end()?;
 
-    // Problem: we want to take from a &str -> &[u8]
-    // Rust Strings are always a UTF-8 sequence
-    // Solution: .as_bytes()
-    rewriter.write(input.as_bytes()).unwrap();
-    rewriter.end().unwrap();
-
-    // An attempt: we thought we could turn our &str into &[u8] with try_into
-    // let output: String = // (&output[..]).try_into().unwrap();
-    println!("{}", String::from_utf8(output).unwrap());
-
-    // Q: Why would we need to have .end() for our streaming rewriter?
+   Ok(String::from_utf8(output)?)
 }
